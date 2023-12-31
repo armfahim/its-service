c07d4879b03db9cda5e5eca7e9d27e96bb88dea0
armfahim <armfahim4010@gmail.com>
2024-01-01 01:42:48 +0600

    test api added

--- src/main/java/com/its/service/resource/auth/AuthenticationResource.java
+++ src/main/java/com/its/service/resource/auth/AuthenticationResource.java
@@ -8,10 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -40,7 +37,7 @@ public class AuthenticationResource {
         service.refreshToken(request, response);
     }
 
-    @PostMapping("/test")
+    @GetMapping("/test")
     public ResponseEntity<Object> test() throws IOException {
         return ok(success("test successfully"));
     }
